f0a409fc090c5b7828125a1c9befe5f01ac8bee9
kjjose00 <kjjose00@gmail.com>
2023-06-21 01:07:21 +0530

    added allowed hosts

--- mercado/settings.py
+++ mercado/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sw0)1)#m)rp-#&yk#%q9q(b+m)&s%@5-ah_w9=h8_8kv09pify'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["mercado-ecommerce.vercel.app"]
 
 
 # Application definition
